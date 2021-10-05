c61f29cd541faa8cd886ff7714003a95509a9f4e
Elias <elias@floreteng.se>
2021-10-05 03:06:53 +0200

    Installer build script to use release

--- build_installer.ps1
+++ build_installer.ps1
@@ -5,5 +5,5 @@ Set-Location ..
 if (Test-Path "${dest}.exe") {
     Remove-Item "${dest}.exe"
 }
-& "C:\Program Files\7-Zip\7z.exe" a $dest -t7z -mmt "-sfxC:\Program Files\7-Zip\7z.sfx" $dir\target\debug\eliasfl-chess-gui.exe $dir\assets\
-& "C:\Program Files\7-Zip\7z.exe" rn "${dest}.exe" $dir\target\debug\eliasfl-chess-gui.exe $dir\eliasfl-chess-gui.exe
+& "C:\Program Files\7-Zip\7z.exe" a $dest -t7z -mmt "-sfxC:\Program Files\7-Zip\7z.sfx" $dir\target\release\eliasfl-chess-gui.exe $dir\assets\
+& "C:\Program Files\7-Zip\7z.exe" rn "${dest}.exe" $dir\target\release\eliasfl-chess-gui.exe $dir\eliasfl-chess-gui.exe
